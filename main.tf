598d245e12ac47b4d169ef1668c6f6d1de9270b1
Bhupeshpos <bhupeshpandey367@gmail.com>
2025-08-05 16:27:23 +0530

    addion

--- main.tf
+++ main.tf
@@ -3,13 +3,8 @@ resource "azurerm_resource_group" "mqt-rg01" {
     location ="eastus" 
 }
 
-resource "azurerm_resource_group" "mqt-rg10" {
-    name = "bhu10"
-    location ="eastus" 
-}
-
-resource "azurerm_resource_group" "mqt-rg11" {
-    name = "bhu11"
+resource "azurerm_resource_group" "mqt-rg02" {
+    name = "bhu2"
     location ="eastus" 
 }
 
@@ -18,6 +13,7 @@ terraform {
     resource_group_name  = "bhui"
     storage_account_name = "mtk12345"
     container_name       = "mtkstg"
+
     key                  = "bhui.tfstate"
   }
 
